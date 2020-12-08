30de3f011dd27f3d9247e4b040f9f4346f17ae62
Tu Nguyen <bin.ngoctu@gmail.com>
2020-12-08 23:02:19 +0700

    Add rest auth

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,6 +72,8 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
+    "rest_auth",
+    "rest_auth.registration",
 ]
 
 LOCAL_APPS = [
@@ -303,7 +305,45 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
+# DRF JWT
+JWT_AUTH = {
+    "JWT_ENCODE_HANDLER":
+    "rest_framework_jwt.utils.jwt_encode_handler",
+
+    "JWT_PAYLOAD_HANDLER":
+    "rest_framework_jwt.utils.jwt_payload_handler",
+
+    # "JWT_DECODE_HANDLER":
+    # "rest_framework_jwt.utils.jwt_decode_handler",
+
+    # "JWT_PAYLOAD_GET_USER_ID_HANDLER":
+    # "rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler",
+
+    # "JWT_RESPONSE_PAYLOAD_HANDLER":
+    # "rest_framework_jwt.utils.jwt_response_payload_handler",
+
+    # "JWT_SECRET_KEY": settings.SECRET_KEY,
+    # "JWT_GET_USER_SECRET_KEY": None,
+    # "JWT_PUBLIC_KEY": None,
+    # "JWT_PRIVATE_KEY": None,
+    # "JWT_ALGORITHM": "HS256",
+    # "JWT_VERIFY": True,
+    # "JWT_VERIFY_EXPIRATION": True,
+    # "JWT_LEEWAY": 0,
+    # "JWT_EXPIRATION_DELTA": datetime.timedelta(seconds=300),
+    # "JWT_AUDIENCE": None,
+    # "JWT_ISSUER": None,
+
+    # "JWT_ALLOW_REFRESH": False,
+    # "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=7),
+
+    # "JWT_AUTH_HEADER_PREFIX": "JWT",
+    # "JWT_AUTH_COOKIE": None,
+}
+
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
+# Combine DRF + JWT + ALLAUTH
+REST_USE_JWT = True
 # Your stuff...
 # ------------------------------------------------------------------------------
