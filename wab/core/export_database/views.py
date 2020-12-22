12978081fe8bc91437a084805edb5bb2a927f8d3
quocnb <quocnguyen1739@gmail.com>
2020-12-22 22:22:45 +0700

    init export file .xlsx & .txt

--- wab/core/export_database/views.py
+++ wab/core/export_database/views.py
@@ -1,4 +1,9 @@
+import io
+from datetime import datetime
+
+import xlsxwriter as xlsxwriter
 from django.db import transaction
+from django.http import HttpResponse
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import AllowAny
 
@@ -32,11 +37,48 @@ class ExportExcelView(CreateAPIView):
     permission_classes = [AllowAny, ]
     serializer_class = SqlFunctionSerializer
 
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         try:
-            sql_functions = SqlFunction.objects.all()
-            serializer = self.get_serializer(sql_functions, many=True)
-            return responses.ok(data=serializer.data, method=constant.GET, entity_name='sql-function')
+            connection_id = kwargs.get("connction")
+            table_name = kwargs.get("table_name")
+            connection = DBProviderConnection.objects.filter(id=connection_id).first()
+            if connection.provider.name == MONGO:
+                mongo_db_manager = MongoDBManager()
+                db = mongo_db_manager.connection_mongo_by_provider(provider_connection=connection)
+                c = db.__getattr__(table_name).find().limit(20)
+
+                result = json.loads(dumps(list(c)))
+                headers = list(result[0].keys())
+
+                output = io.BytesIO()
+                workbook = xlsxwriter.Workbook(output)
+                worksheet = workbook.add_worksheet()
+
+                cell_format_header = workbook.add_format()
+                cell_format_header.set_bold()
+
+                for index in range(len(headers)):
+                    worksheet.write(0, index, headers[index], cell_format_header)
+
+                for row_num, columns in enumerate(result):
+                    for index in range(len(headers)):
+                        value = columns.get(headers[index]) if index != 0 else columns.get(headers[index]).get('$oid')
+                        worksheet.write(row_num + 1, index, value)
+
+                workbook.close()
+
+                output.seek(0)
+
+                today = datetime.now().strftime("%d/%m/%Y_%H%M%S")
+                filename = f"ExportData-{table_name}-{today}.xlsx"
+                response = HttpResponse(
+                    output,
+                    content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                )
+                response['Content-Disposition'] = 'attachment; filename=%s' % filename
+
+                return response
+            return responses.bad_request(data=None, message_code="SQL_PROVIDER_NOT_FOUND")
         except Exception as err:
             return responses.not_found(data=None, message_code='SQL_FUNCTION_NOT_FOUND', message_system=err)
 
@@ -46,10 +88,45 @@ class ExportTextView(CreateAPIView):
     permission_classes = [AllowAny, ]
     serializer_class = SqlFunctionSerializer
 
-    def post(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         try:
-            sql_functions = SqlFunction.objects.all()
-            serializer = self.get_serializer(sql_functions, many=True)
-            return responses.ok(data=serializer.data, method=constant.GET, entity_name='sql-function')
+            connection_id = kwargs.get("connction")
+            table_name = kwargs.get("table_name")
+            connection = DBProviderConnection.objects.filter(id=connection_id).first()
+            if connection.provider.name == MONGO:
+                mongo_db_manager = MongoDBManager()
+                db = mongo_db_manager.connection_mongo_by_provider(provider_connection=connection)
+                c = db.__getattr__(table_name).find().limit(20)
+
+                result = json.loads(dumps(list(c)))
+                headers = list(result[0].keys())
+
+                content = ''
+                for header in headers:
+                    content += header
+                    if headers.index(header) != len(headers) - 1:
+                        content += ', '
+                    else:
+                        content += '\n'
+
+                for value in result:
+                    for header in headers:
+                        if header == "_id":
+                            content += value.get(header).get('$oid')
+                        else:
+                            content += value.get(header)
+                        if headers.index(header) != len(headers) - 1:
+                            content += ', '
+                        else:
+                            content += '\n'
+
+
+                today = datetime.now().strftime("%d/%m/%Y_%H%M%S")
+                filename = f"ExportData-{table_name}-{today}.txt"
+                response = HttpResponse(content, content_type='text/plain')
+                response['Content-Disposition'] = 'attachment; filename=%s' % filename
+
+                return response
+            return responses.bad_request(data=None, message_code="SQL_PROVIDER_NOT_FOUND")
         except Exception as err:
             return responses.not_found(data=None, message_code='SQL_FUNCTION_NOT_FOUND', message_system=err)
