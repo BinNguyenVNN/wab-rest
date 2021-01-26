6479f9d1f5bb70e9c04d144b838bd3a03f01a9b5
le sinh <lslesinh@gmail.com>
2021-01-26 16:37:16 +0700

    Add api get sql function detail

--- wab/core/sql_function/urls.py
+++ wab/core/sql_function/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from wab.core.sql_function.api.views import SqlFunctionListView, SqlFunctionCreateView, SqlFunctionUpdateView, \
-    SqlFunctionDeleteView, SqlJoinViewTest, SqlUnionViewTest, PreviewSqlFunctionView
+    SqlFunctionDeleteView, SqlJoinViewTest, SqlUnionViewTest, PreviewSqlFunctionView, SqlFunctionDetailView
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -15,6 +15,7 @@ urlpatterns = [
                   path('test-union/', SqlUnionViewTest.as_view(), name='SqlUnionViewTest'),
                   path('list/', SqlFunctionListView.as_view(), name='SqlFunctionListView'),
                   path('create/', SqlFunctionCreateView.as_view(), name='SqlFunctionCreateView'),
+                  path('detail/<int:pk>/', SqlFunctionDetailView.as_view(), name='SqlFunctionUpdateView'),
                   path('update/<int:pk>/', SqlFunctionUpdateView.as_view(), name='SqlFunctionUpdateView'),
                   path('delete/<int:pk>/', SqlFunctionDeleteView.as_view(), name='SqlFunctionDeleteView'),
                   path('preview/<int:pk>/', PreviewSqlFunctionView.as_view(),
